e43cfa278efa278a5d975ab6bfdbb927b2f64bd8
cesargalindo <cesar.galindo@gmail.com>
2017-08-18 08:36:04 -0700

    Get Cordova working.

--- client/main.ts
+++ client/main.ts
@@ -1,10 +1,17 @@
 import 'angular2-meteor-polyfills';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Meteor } from "meteor/meteor";
+import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './imports/app/app.module';
 
 import '../both/methods/parties.methods';
 
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+// const platform = platformBrowserDynamic();
+// platform.bootstrapModule(AppModule);
+enableProdMode();
+
+Meteor.startup(() => {
+    platformBrowserDynamic().bootstrapModule(AppModule);
+});
